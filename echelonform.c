fd632b6934db063ddfbb5d0ec7e32f220fa5156c
hakankaraagac <hakankaraagacc@gmail.com>
2016-12-08 19:07:28 +0200

    Yorumda ki türkce karakterin duzeltimi

--- echelonform.c
+++ echelonform.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	float matris[3][3]={1,2,3, 2,3,2, 3,1,4}; // Kendi kafama göre bir matris tanýmladým, 3*3 lük bir matrisi kullanýcýdan 
+	float matris[3][3]={1,2,3, 2,3,2, 3,1,4}; // Kendi kafama göre bir matris tanýmladým, 3*3 lük bir matrisi kullanicidan 
 	//girmesini de isteyebilirdim.
 	int i;
 	int j;
@@ -16,23 +16,23 @@ int main()
 	    printf("\n");
 }
 		
-   katsayi1=(matris[1][0])/(matris[0][0]); //katsayý ki bu sayý ile 0 yapýcaz.
-      printf("\nkatsayi1  %f  \n",katsayi1); //burada katsayýyý görüyoruz.
-      matris[1][0]=((matris[0][0])*katsayi1)-matris[1][0]; //bu iþlemde ise 2.satirýn 1. elemanýný sýfýr yapýyoruz.
+   katsayi1=(matris[1][0])/(matris[0][0]); //katsayý ki bu sayi ile 0 yapýcaz.
+      printf("\nkatsayi1  %f  \n",katsayi1); //burada katsayiyi görüyoruz.
+      matris[1][0]=((matris[0][0])*katsayi1)-matris[1][0]; //bu islemde ise 2.satirin 1. elemanini sýfýr yapýyoruz.
       matris[1][1]=((matris[0][1])*katsayi1)-matris[1][1]; // 2.satýr 2.eleman için
       matris[1][2]=((matris[0][2])*katsayi1)-matris[1][2]; //   ""    3.  ""     ""
       
       
 	katsayi2=(matris[2][0])/(matris[0][0]);        
 	printf("\nkatsayi2  %f  \n",katsayi2);
-	matris[2][0]=((matris[0][0])*katsayi2)-matris[2][0]; //ayni iþlemler 3.satirin ilk elemanini 0 yapmak için
+	matris[2][0]=((matris[0][0])*katsayi2)-matris[2][0]; //ayni islemler 3.satirin ilk elemanini 0 yapmak için
 	matris[2][1]=((matris[0][1])*katsayi2)-matris[2][1];
 	matris[2][2]=((matris[0][2])*katsayi2)-matris[2][2];
 	
 	
 	katsayi3=(matris[2][1])/(matris[1][1]);
 	printf("\nkatsayi3  %f\n",katsayi3);
-	matris[2][1]=((matris[1][1])*katsayi3)-matris[2][1]; //3. satirin ikinci elamanin 0 yapma iþlemi//
+	matris[2][1]=((matris[1][1])*katsayi3)-matris[2][1]; //3. satirin ikinci elamanin 0 yapma islemi//
 	matris[2][2]=((matris[1][2])*katsayi3)-matris[2][2];
 	matris[2][2]=matris[2][2]/matris[2][2];
 	printf("\n echelon form of matrix\n");
